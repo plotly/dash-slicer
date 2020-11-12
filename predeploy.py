7b95eef334d79c57f666c42b38b4f102f742dc65
Emmanuelle Gouillart <emma@plot.ly>
2020-11-12 09:48:03 +0100

    black

--- predeploy.py
+++ predeploy.py
@@ -2,7 +2,7 @@ import shutil
 import subprocess
 
 # Install dash-slicer from dev version
-subprocess.run('python -m pip install -e .'.split(' '))
+subprocess.run("python -m pip install -e .".split(" "))
 # Use requirements of app
-shutil.copy('test_deploy/requirements.txt', 'requirements.txt')
-subprocess.run('python -m pip install -r requirements.txt'.split(' '))
+shutil.copy("test_deploy/requirements.txt", "requirements.txt")
+subprocess.run("python -m pip install -r requirements.txt".split(" "))
