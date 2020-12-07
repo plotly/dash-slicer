82f297040e8e6e1f6e794151220667f2cb1971d8
Emmanuelle Gouillart <emma@plot.ly>
2020-12-07 13:57:30 +0100

    try to make black happy

--- tests/test_slicer.py
+++ tests/test_slicer.py
@@ -39,12 +39,12 @@ def test_slicer_thumbnail():
     app = dash.Dash()
     vol = np.random.uniform(0, 255, (100, 100, 100)).astype(np.uint8)
 
-    s = VolumeSlicer(app, vol)
+    _ = VolumeSlicer(app, vol)
     # Test for name pattern of server-side callback when thumbnails are used
     assert any(["server-data.data" in key for key in app.callback_map])
 
     app = dash.Dash()
-    s = VolumeSlicer(app, vol, thumbnail=False)
+    _ = VolumeSlicer(app, vol, thumbnail=False)
     # No server-side callbacks when no thumbnails are used
     assert not any(["server-data.data" in key for key in app.callback_map])
 
