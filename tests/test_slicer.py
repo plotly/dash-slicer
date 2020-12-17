994ac1b523b978b58d1dee3004204e17ac06fde5
Almar Klein <almar.klein@gmail.com>
2020-12-17 16:13:28 +0100

    add more tests

--- tests/test_slicer.py
+++ tests/test_slicer.py
@@ -11,6 +11,10 @@ def test_slicer_init():
 
     vol = np.random.uniform(0, 255, (100, 100, 100)).astype(np.uint8)
 
+    # Need a dash app
+    with raises(TypeError):
+        VolumeSlicer("not a dash app", vol)
+
     # Need a valid volume
     with raises(TypeError):
         VolumeSlicer(app, [3, 4, 5])
@@ -22,13 +26,18 @@ def test_slicer_init():
         VolumeSlicer(app, vol, axis=4)
 
     # Need a valide thumbnail
-    with raises(ValueError):
+    with raises(TypeError):
         VolumeSlicer(app, vol, thumbnail=20.2)
+
+    # Need a valide scene id
+    with raises(TypeError):
+        VolumeSlicer(app, vol, scene_id=20)
 
     # This works
     s = VolumeSlicer(app, vol)
 
     # Check properties
+    assert s.axis == 0
     assert isinstance(s.graph, dcc.Graph)
     assert isinstance(s.slider, dcc.Slider)
     assert isinstance(s.stores, list)
@@ -50,6 +59,33 @@ def test_slicer_thumbnail():
     # No server-side callbacks when no thumbnails are used
     assert not any(["server-data.data" in key for key in app.callback_map])
 
+    # Default thumbnail size
+    s = VolumeSlicer(app, vol)
+    assert s._slice_info["thumbnail_size"] == (32, 32)
+
+    # Custom size
+    s = VolumeSlicer(app, vol, thumbnail=20)
+    assert s._slice_info["thumbnail_size"] == (20, 20)
+    s = VolumeSlicer(app, vol, thumbnail=50)
+    assert s._slice_info["thumbnail_size"] == (50, 50)
+
+    # Custom but too big
+    s = VolumeSlicer(app, vol, thumbnail=102)
+    assert s._slice_info["thumbnail_size"] == (100, 100)
+
+    # Disable
+    s = VolumeSlicer(app, vol, thumbnail=False)
+    assert s._slice_info["thumbnail_size"] == (100, 100)
+    s = VolumeSlicer(app, vol, thumbnail=0)
+    assert s._slice_info["thumbnail_size"] == (100, 100)
+    s = VolumeSlicer(app, vol, thumbnail=-1)
+    assert s._slice_info["thumbnail_size"] == (100, 100)
+
+    # None is simply not allowed - removes ambiguity about
+    # whether None means no-thumbnails, or the default size
+    with raises(TypeError):
+        VolumeSlicer(app, vol, thumbnail=None)
+
 
 def test_clim():
     app = dash.Dash()
@@ -64,6 +100,12 @@ def test_clim():
 
     s = VolumeSlicer(app, vol, clim=(10, 12))
     assert s._initial_clim == (10, 12)
+
+    # Fails
+    with raises(TypeError):
+        VolumeSlicer(app, vol, clim=10)
+    with raises(TypeError):
+        VolumeSlicer(app, vol, clim=(10, 12, 14))
 
 
 def test_scene_id_and_context_id():
@@ -82,6 +124,26 @@ def test_scene_id_and_context_id():
     assert s1._context_id != s2._context_id and s1._context_id != s3._context_id
 
 
+def test_slice():
+    app = dash.Dash()
+    vol = np.random.uniform(0, 255, (10, 20, 30)).astype(np.uint8)
+
+    s = VolumeSlicer(app, vol, axis=0)
+    im = s._slice(1, (0, 100))
+    assert im.dtype == np.uint8
+    assert im.shape == (20, 30)
+
+    s = VolumeSlicer(app, vol, axis=1)
+    im = s._slice(1, (0, 100))
+    assert im.dtype == np.uint8
+    assert im.shape == (10, 30)
+
+    s = VolumeSlicer(app, vol, axis=2)
+    im = s._slice(1, (0, 100))
+    assert im.dtype == np.uint8
+    assert im.shape == (10, 20)
+
+
 def test_create_overlay_data():
 
     app = dash.Dash()
@@ -95,6 +157,16 @@ def test_create_overlay_data():
 
     # Bool overlay - with color
     overlay = s.create_overlay_data(vol > 10, "#ff0000")
+    assert isinstance(overlay, list) and len(overlay) == s.nslices
+    assert all(isinstance(x, str) for x in overlay)
+
+    # Bool overlay - with color rgb
+    overlay = s.create_overlay_data(vol > 10, [0, 255, 0])
+    assert isinstance(overlay, list) and len(overlay) == s.nslices
+    assert all(isinstance(x, str) for x in overlay)
+
+    # Bool overlay - with color rgba
+    overlay = s.create_overlay_data(vol > 10, [0, 255, 0, 100])
     assert isinstance(overlay, list) and len(overlay) == s.nslices
     assert all(isinstance(x, str) for x in overlay)
 
@@ -114,6 +186,12 @@ def test_create_overlay_data():
     assert all(x is None for x in overlay)
 
     # Wrong
+    with raises(ValueError):
+        s.create_overlay_data(vol > 10, "red")  # named colors not supported yet
+    with raises(ValueError):
+        s.create_overlay_data(vol > 10, [0, 255, 0, 100, 100])  # not a color
+    with raises(ValueError):
+        s.create_overlay_data(vol > 10, [0, 255])  # not a color
     with raises(TypeError):
         s.create_overlay_data("not a valid mask")
     with raises(ValueError):
