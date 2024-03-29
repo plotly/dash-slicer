fba8ffd62e85b10db0137576ef8359a5ca985e7a
Almar Klein <almar@almarklein.org>
2024-03-29 09:29:11 +0100

    bump version

--- dash_slicer/__init__.py
+++ dash_slicer/__init__.py
@@ -5,5 +5,5 @@ dash_slicer - a volume slicer for Dash
 from .slicer import VolumeSlicer  # noqa: F401
 
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 version_info = tuple(map(int, __version__.split(".")))
